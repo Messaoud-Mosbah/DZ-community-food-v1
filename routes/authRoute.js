28b0ac13b299c5c5e31d76dccbead95ebedca645
Mosbah Messaoud <m.mosbah@esi-sba.dz>
2026-02-03 14:29:31 +0100

    first commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const {
-  signupValidator,
-  loginValidator,
-} = require('../utils/validators/authValidators');
 
-const {
-  signup,
-  login,
-  
-} = require('../services/authService');
-
-const router = express.Router();
-
-router.post('/signup', signupValidator, signup);
-router.post('/login', loginValidator, login);
-
-module.exports = router;
