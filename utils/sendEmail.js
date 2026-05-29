6b78def5b30ff7d156f71269e14f14a64b052e7e
Mosbah Messaoud <m.mosbah@esi-sba.dz>
2026-05-29 23:53:42 +0100

    d

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,12 +4,10 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ email, subject, message, html }) => {
   try {
-    // 💡 إجبار السيرفر على الإرسال لإيميلك الشخصي الموثق في الساندبوكس لتجاوز المشكلة تماماً
-    const recipientEmail = 'm.mosbah@esi-sba.dz';
 
     const { data, error } = await resend.emails.send({
       from: 'Feed Me <onboarding@resend.dev>', // تأكد أنها تنتهي بـ resend.dev وليس feedme.com
-      to: recipientEmail,
+      to: email,
       subject: subject,
       text: message,
       html: html
